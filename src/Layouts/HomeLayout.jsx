dd30205a14719445258213cdff5ee522663849b8
RafexStrike <adnanrahmanrafi515@gmail.com>
2025-05-05 10:25:06 +0600

    footer has been added

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from '../Components/Navbar/Navbar';
 import { Outlet } from 'react-router';
+import Footer from '../Components/Footer/Footer';
 
 const HomeLayout = () => {
     return (
-        <div className='max-w-6xl mx-auto mt-4'>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
+        <div className='min-h-screen flex flex-col'>
+            <div className='max-w-6xl mx-auto mt-4 px-4 flex-grow w-full'>
+                <Navbar></Navbar>
+                <Outlet></Outlet>
+            </div>
+            <Footer />
         </div>
     );
 };
