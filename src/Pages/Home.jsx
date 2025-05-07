13e40fd028b594adae9c42832f1ec44b44165dcf
RafexStrike <adnanrahmanrafi515@gmail.com>
2025-05-07 21:50:06 +0600

    title and descriptions animations of the Home Page sections - issue fixed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,28 +1,41 @@
-import React, { useRef } from 'react';
-import EventSponsorshipLayout from '../Components/EventSponsorship/EventSponsorship';
-import SuccessStories from '../Components/SuccessStories/SuccessStories';
-import UpcomingEvents from '../Components/UpcomingEvents/UpcomingEvents';
-import Slider from '../Components/Slider/Slider';
-import DynamicPageTitle from '../Components/DynamicPageTitle/DynamicPageTitle';
-import SwiperTest from '../Components/SwiperTest/SwiperTest';
+import React, { useEffect, useRef } from "react";
+import EventSponsorshipLayout from "../Components/EventSponsorship/EventSponsorship";
+import SuccessStories from "../Components/SuccessStories/SuccessStories";
+import UpcomingEvents from "../Components/UpcomingEvents/UpcomingEvents";
+import Slider from "../Components/Slider/Slider";
+import DynamicPageTitle from "../Components/DynamicPageTitle/DynamicPageTitle";
+import SwiperTest from "../Components/SwiperTest/SwiperTest";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
-    const eventsRef = useRef(null);
-    const handleScrollToEvents = () => {
-        eventsRef.current?.scrollIntoView({ behavior: 'smooth' });
-    };
-    return (
-        <div>
-            <DynamicPageTitle title="Home" />
-            <Slider onJoinNowClick={handleScrollToEvents} />
-            {/* <SwiperTest /> */}
-           <div ref={eventsRef}>
-            <UpcomingEvents />
-           </div>
-            <EventSponsorshipLayout />
-            <SuccessStories />
-        </div>
-    );
+  const eventsRef = useRef(null);
+  const handleScrollToEvents = () => {
+    eventsRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    AOS.init({ once: false, duration: 800 });
+
+    const timeout = setTimeout(() => {
+      AOS.refresh();
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <div>
+      <DynamicPageTitle title="Home" />
+      <Slider onJoinNowClick={handleScrollToEvents} />
+      {/* <SwiperTest /> */}
+      <div ref={eventsRef}>
+        <UpcomingEvents />
+      </div>
+      <EventSponsorshipLayout />
+      <SuccessStories />
+    </div>
+  );
 };
 
 export default Home;
